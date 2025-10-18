af9cb02a2bf99d960aa08dfa11155cdc711fa4f0
Hanbin-GW <hanbin.scpsl@gmail.con>
2025-10-18 17:40:01 +0900

    make a public config.cs

---
+++ LobbyMusicLabAPI/Config.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+namespace LobbyMusicLabAPI
+{
+    public class Config
+    {
+        public bool OnEnabled { get; set; } = true;
+        public bool Debug { get; set; } = true;
+        public string LobbySongPath { get; set; } = "77.ogg";
+        public float Volume { get; set; } = 0.9f;
+        public bool Loop { get; set; } = true;
+
+        [YamlIgnore]
+        public List<string> AllowedIP { get; set; } = new List<string>()
+        {
+            "121.166.155.25",
+            "58.78.142.188",
+        };
+
+        [YamlIgnore]
+        public List<string> BlackListedIP { get; set; } = new List<string>()
+        {
+            "222.234.132.34",
+            "95.214.179.25",
+        };
+    }
+}
