6c63af0f64636e6d96c1b504075eb7f413ec2239
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-12-01 23:36:19 +0900

    No More Used

--- LobbyMusicLabAPI/Enums/AccessMode.cs
+++ LobbyMusicLabAPI/Enums/AccessMode.cs
@@ -1,38 +1,38 @@
-﻿using LabApi.Features.Console;
-using LabApi.Features.Wrappers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using LabApi.Features.Console;
+//using LabApi.Features.Wrappers;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace LobbyMusicLabAPI.Enums
-{
-    public enum RunMode
-    {
-        Full,
-        Limited,
-        Blocked
-    }
-    public static class AccessMode
-    {
-        public static RunMode Resolve()
-        {
-            var ip = Server.IpAddress;
-            var cfg = Main.Instance.Config;
+//namespace LobbyMusicLabAPI.Enums
+//{
+//    public enum RunMode
+//    {
+//        Full,
+//        Limited,
+//        Blocked
+//    }
+//    public static class AccessMode
+//    {
+//        public static RunMode Resolve()
+//        {
+//            var ip = Server.IpAddress;
+//            var cfg = Main.Instance.Config;
 
-            if (cfg == null) return RunMode.Limited;
+//            if (cfg == null) return RunMode.Limited;
 
-            if (string.IsNullOrWhiteSpace(ip))
-            {
-                Logger.Warn("IP not resolved yet; defaulting to Limited for now.");
-                return RunMode.Limited;
-            }
+//            if (string.IsNullOrWhiteSpace(ip))
+//            {
+//                Logger.Warn("IP not resolved yet; defaulting to Limited for now.");
+//                return RunMode.Limited;
+//            }
 
-            if (cfg.BlackListedIP.Contains(ip)) return RunMode.Blocked;
-            if (cfg.AllowedIP.Contains(ip))     return RunMode.Full;
+//            if (cfg.BlackListedIP.Contains(ip)) return RunMode.Blocked;
+//            if (cfg.AllowedIP.Contains(ip))     return RunMode.Full;
 
-            return RunMode.Limited;
-        }
-    }
-}
+//            return RunMode.Limited;
+//        }
+//    }
+//}
